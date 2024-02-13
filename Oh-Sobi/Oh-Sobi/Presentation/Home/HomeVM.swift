747b94832b01f6bd8951f1b91c19c2aefd7ab7ab
Doogie <chris_97@naver.com>
2024-02-14 01:03:28 +0900

    feat: 월간 소비 호출 기능 구현

--- Oh-Sobi/Oh-Sobi/Presentation/Home/HomeVM.swift
+++ Oh-Sobi/Oh-Sobi/Presentation/Home/HomeVM.swift
@@ -5,12 +5,35 @@
 //  Created by Doogie on 2/12/24.
 //
 
-protocol HomeVMable: HomeVMInput, HomeVMOutput, AnyObject {}
+import Foundation
 
-protocol HomeVMInput {}
+protocol HomeVMable: HomeVMInput, HomeVMOutput {}
+
+protocol HomeVMInput {
+    func getMonthlyConsumption()
+}
 
 protocol HomeVMOutput {}
 
 final class HomeVM: HomeVMable {
+    private let getMonthlyConsumptionUseCase: GetMonthlyConsumptionUseCase
     
+    init(getMonthlyConsumptionUseCase: GetMonthlyConsumptionUseCase) {
+        self.getMonthlyConsumptionUseCase = getMonthlyConsumptionUseCase
+    }
+    
+    func getMonthlyConsumption() {
+        let date = Date()
+
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: date)
+        guard let year = components.year,
+              let month = components.month else {
+            debugPrint("월간 소비 호출 에러")
+            return
+        }
+        let monthlyConsumption = getMonthlyConsumptionUseCase.execute(year: year, month: month)
+        
+        print(monthlyConsumption)
+    }
 }
