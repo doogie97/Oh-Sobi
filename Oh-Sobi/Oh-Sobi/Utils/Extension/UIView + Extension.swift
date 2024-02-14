f5f3eef55dac4b3c6152598bfabb3fee74bde0a9
Doogie <chris_97@naver.com>
2024-02-14 23:24:00 +0900

    feat: inset관련 메서드 구현

--- Oh-Sobi/Oh-Sobi/Utils/Extension/UIView + Extension.swift
+++ Oh-Sobi/Oh-Sobi/Utils/Extension/UIView + Extension.swift
@@ -7,7 +7,32 @@
 
 import UIKit
 extension UIView {
-    func pretendardLabel(family: UIFont.Family = .Regular, size: CGFloat = 14, color: UIColor = .black, lineCount: Int = 1, text: String? = "", textAlignment: NSTextAlignment = .left) -> UILabel {
+    enum Direction {
+        case height
+        case width
+    }
+    
+    func margin(_ direction: Direction, _ margine: Double) -> Double {
+        let designHeight: Double = 852
+        let designWidth: Double = 393
+        
+        switch direction {
+        case .height:
+            return UIScreen.main.bounds.height * margine/designHeight
+        case .width:
+            return UIScreen.main.bounds.width * margine/designWidth
+        }
+    }
+    
+    var isSE: Bool {
+        return UIScreen.main.bounds.height <= 667
+    }
+    
+    var isMini: Bool {
+        return UIScreen.main.bounds.width <= 375 && UIScreen.main.bounds.height > 667
+    }
+    
+    func pretendardLabel(family: UIFont.Family = .Regular, size: CGFloat = 14, color: UIColor = .mainBlack, lineCount: Int = 1, text: String? = "", textAlignment: NSTextAlignment = .left) -> UILabel {
         let label = UILabel()
         label.font = .pretendard(family, size)
         label.textColor = color
