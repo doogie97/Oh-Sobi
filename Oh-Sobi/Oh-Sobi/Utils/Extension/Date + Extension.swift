f381b57e6203d2a8eb5c7df7ae53293bad7544b6
Doogie <chris_97@naver.com>
2024-02-15 23:23:22 +0900

    feat: date의 연월일 구하는 메서드 구현

--- Oh-Sobi/Oh-Sobi/Utils/Extension/Date + Extension.swift
+++ Oh-Sobi/Oh-Sobi/Utils/Extension/Date + Extension.swift
@@ -15,4 +15,19 @@ extension Date {
         }
         return range.count
     }
+    
+    struct YearMonthDay {
+        let year: Int
+        let month: Int
+        let day: Int
+    }
+    
+    func yearMonthDay() -> YearMonthDay {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: self)
+        
+        return YearMonthDay(year: components.year ?? 0,
+                            month: components.month ?? 0,
+                            day: components.day ?? 0)
+    }
 }
