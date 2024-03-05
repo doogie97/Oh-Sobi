805c853635010a7f3b3ff68ec5967d0e80e60806
Doogie <chris_97@naver.com>
2024-03-05 21:45:54 +0900

    feat: 주간 소비 section view 구현(일일 소비 구현 예정)

--- Oh-Sobi/Oh-Sobi/Presentation/Home/HomeView/HomeSectionView/WeeklySectionCVCell.swift
+++ Oh-Sobi/Oh-Sobi/Presentation/Home/HomeView/HomeSectionView/WeeklySectionCVCell.swift
@@ -9,7 +9,48 @@ import UIKit
 import SnapKit
 
 final class WeeklySectionCVCell: UICollectionViewCell {
+    private lazy var titleLabel = pretendardLabel(family: .Bold, size: 16, text: "주간 소비")
+    private lazy var rightIndicator = {
+        let indicator = UIImage(systemName: "chevron.right")?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: indicator)
+        imageView.tintColor = .mainBlack
+        
+        return imageView
+    }()
+    
+    private lazy var moveCalendarButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(touchMoveCalendarButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func touchMoveCalendarButton() {
+        print("touch moveCalendarButton")
+    }
+    
+    private lazy var weeklyStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    private func setWeeklyStackView(weeklyConsumption: [DailyConsumption?]) {
+        for index in 0..<7 {
+            let testView = UIView()
+            if index % 2 == 0 {
+                testView.backgroundColor = .red
+            } else {
+                testView.backgroundColor = .blue
+            }
+            
+            weeklyStackView.addArrangedSubview(testView)
+        }
+    }
+    
     func setCellContents() {
+        setWeeklyStackView(weeklyConsumption: [])
         if self.contentView.subviews.isEmpty {
             setLayout()
         }
@@ -18,6 +59,33 @@ final class WeeklySectionCVCell: UICollectionViewCell {
     private func setLayout() {
         self.contentView.backgroundColor = .ivory
         self.contentView.layer.cornerRadius = 12
+        
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(rightIndicator)
+        self.contentView.addSubview(weeklyStackView)
+        self.contentView.addSubview(moveCalendarButton)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(margin(.width, 16))
+        }
+        
+        rightIndicator.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel)
+            $0.trailing.equalToSuperview().inset(margin(.width, 24))
+            $0.height.equalTo(16)
+            $0.width.equalTo(8)
+        }
+        
+        weeklyStackView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).inset(margin(.height, -16))
+            $0.leading.trailing.equalToSuperview().inset(margin(.width, 10))
+            $0.bottom.equalToSuperview().inset(margin(.height, 16))
+        }
+        
+        moveCalendarButton.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(weeklyStackView.snp.top)
+        }
     }
     
     override func prepareForReuse() {
